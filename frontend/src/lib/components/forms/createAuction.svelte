39e65260ce9ffce8436aeb74fc920df876b5219a
crthpl <crthpl@gmail.com>
2025-04-22 13:50:33 -0700

    add initial auction frontend

---
+++ frontend/src/lib/components/forms/createAuction.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { sendClientMessage } from '$lib/api.svelte';
+	import { buttonVariants } from '$lib/components/ui/button';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { websocket_api } from 'schema-js';
+	import { protoSuperForm } from './protoSuperForm';
+	const initialData = websocket_api.CreateMarket.create({
+		name: '',
+		description: '',
+		minSettlement: 0,
+		maxSettlement: 0.01
+	});
+	let open = $state(false);
+
+	const form = protoSuperForm(
+		'create-market',
+		websocket_api.CreateMarket.fromObject,
+		(createMarket) => {
+			createMarket.name = '[AUCTION] ' + createMarket.name;
+			sendClientMessage({ createMarket });
+			open = false;
+		},
+		initialData
+	);
+
+	const { form: formData, enhance } = form;
+</script>
+
+<Dialog.Root bind:open>
+	<Dialog.Trigger class={buttonVariants({ variant: 'default', className: 'text-base' })}
+		>Create Auction</Dialog.Trigger
+	>
+	<Dialog.Content>
+		<form use:enhance>
+			<Dialog.Header>
+				<Dialog.Title>Create Auction</Dialog.Title>
+			</Dialog.Header>
+			<Form.Field {form} name="name">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Name</Form.Label>
+						<Input {...props} bind:value={$formData.name} />
+					{/snippet}
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+			<Form.Field {form} name="description">
+				<Form.Control>
+					{#snippet children({ props })}
+						<Form.Label>Description</Form.Label>
+						<Input {...props} bind:value={$formData.description} />
+					{/snippet}
+				</Form.Control>
+				<Form.FieldErrors />
+			</Form.Field>
+			<Dialog.Footer>
+				<Form.Button>Submit</Form.Button>
+			</Dialog.Footer>
+		</form>
+	</Dialog.Content>
+</Dialog.Root>
