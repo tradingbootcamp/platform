8759d8c5fc973ef77dcfd2a31ee1d561340123c1
mahna <mahna@mahna.com>
2025-04-22 17:55:57 -0400

    Changed Auction view to a grid display

--- frontend/src/routes/auction/+page.svelte
+++ frontend/src/routes/auction/+page.svelte
@@ -7,9 +7,12 @@
 <div class="mr-auto flex flex-col gap-8 pt-8">
 	<h1 class="text-xl font-bold">Auction</h1>
 	<CreateAuction />
-	{#each Array.from(serverState.markets.values())
-		.filter((a) => a.definition?.name.startsWith('[AUCTION] '))
-		.sort((a, b) => (a.definition?.transactionTimestamp ?? 0) - (b.definition?.transactionTimestamp ?? 0)) as market}
-		<AuctionLink market={market.definition} />
-	{/each}
+
+	<div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		{#each Array.from(serverState.markets.values())
+			.filter((a) => a.definition?.name.startsWith('[AUCTION] '))
+			.sort((a, b) => (a.definition?.transactionTimestamp ?? 0) - (b.definition?.transactionTimestamp ?? 0)) as market}
+			<AuctionLink market={market.definition} />
+		{/each}
+	</div>
 </div>
