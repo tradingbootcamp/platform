8fb2e37a9548c1c91458af97fb8c8cd72ae5a218
crthpl <theo@reinsberg.info>
2025-06-06 18:30:51 -0700

    fix default value for buyer_id 3

--- backend/migrations/20250606152654_add_buyer_id_to_auction.sql
+++ backend/migrations/20250606152654_add_buyer_id_to_auction.sql
@@ -1,2 +1,2 @@
 -- Add buyer_id column to auction table
-ALTER TABLE "auction" ADD COLUMN "buyer_id" INTEGER DEFAULT 0 NOT NULL REFERENCES "account";
+ALTER TABLE "auction" ADD COLUMN "buyer_id" INTEGER DEFAULT 0 NOT NULL;
