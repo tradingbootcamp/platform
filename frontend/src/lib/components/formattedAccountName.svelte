ecac2437e53586f271b7d770f6d7067bcf524ec2
crthpl <theo@reinsberg.info>
2025-12-26 11:03:00 -0800

    OOPS

---
+++ frontend/src/lib/components/formattedAccountName.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { accountName, isAltAccount } from '$lib/api.svelte';
+
+	let {
+		accountId,
+		me = undefined
+	}: { accountId: number | null | undefined; me?: string } = $props();
+
+	let name = $derived(accountName(accountId, me));
+	let isAlt = $derived(isAltAccount(accountId));
+</script>
+
+<span class:italic={isAlt}>{name}</span>
