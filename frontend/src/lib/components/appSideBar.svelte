e5021145f58ea7fea0576b25750d14fd354f2ace
Brian <briantsmiley42@gmail.com>
2025-12-26 18:05:23 -0800

    adds tooltip to create market button

--- frontend/src/lib/components/appSideBar.svelte
+++ frontend/src/lib/components/appSideBar.svelte
@@ -76,10 +76,7 @@
 		>
 			<Sidebar.Menu class="!w-auto">
 				<Sidebar.MenuItem>
-					<Sidebar.MenuButton
-						class="!w-8 !h-8 !p-2"
-						onclick={() => sidebarState.toggle()}
-					>
+					<Sidebar.MenuButton class="!h-8 !w-8 !p-2" onclick={() => sidebarState.toggle()}>
 						<PanelLeft />
 					</Sidebar.MenuButton>
 				</Sidebar.MenuItem>
@@ -88,7 +85,7 @@
 				class={cn(
 					'absolute !w-8',
 					sidebarState.state === 'expanded'
-						? 'animate-home-expand !w-[calc(100%-2.5rem)]'
+						? '!w-[calc(100%-2.5rem)] animate-home-expand'
 						: 'animate-home-collapse'
 				)}
 			>
@@ -101,7 +98,7 @@
 								<span
 									class={cn(
 										'ml-3 whitespace-nowrap transition-opacity duration-200',
-										sidebarState.state === 'collapsed' && 'opacity-0 w-0 overflow-hidden'
+										sidebarState.state === 'collapsed' && 'w-0 overflow-hidden opacity-0'
 									)}
 								>
 									Home
@@ -128,13 +125,21 @@
 								</a>
 							{/snippet}
 						</Sidebar.MenuButton>
-						<Sidebar.MenuAction
-							class="bg-primary text-primary-foreground opacity-50 hover:opacity-100 hover:bg-primary/90 hover:text-primary-foreground"
-						>
-							{#snippet child({ props })}
-								<CreateMarket {...props} onclick={handleClick}><Plus /></CreateMarket>
-							{/snippet}
-						</Sidebar.MenuAction>
+						<Tooltip.Root>
+							<Tooltip.Trigger>
+								{#snippet child({ props: tooltipProps })}
+									<Sidebar.MenuAction
+										class="bg-primary text-primary-foreground opacity-50 hover:bg-primary/90 hover:text-primary-foreground hover:opacity-100"
+										{...tooltipProps}
+									>
+										{#snippet child({ props })}
+											<CreateMarket {...props} onclick={handleClick}><Plus /></CreateMarket>
+										{/snippet}
+									</Sidebar.MenuAction>
+								{/snippet}
+							</Tooltip.Trigger>
+							<Tooltip.Content side="right">Create Market</Tooltip.Content>
+						</Tooltip.Root>
 						{#if allStarredMarkets().length > 0}
 							<Sidebar.MenuSub>
 								{#each allStarredMarkets() as marketId}
@@ -152,7 +157,11 @@
 													onclick={handleClick}
 													class="ml-4"
 												>
-													<span>{formatMarketName(serverState.markets.get(marketId)?.definition.name)}</span>
+													<span
+														>{formatMarketName(
+															serverState.markets.get(marketId)?.definition.name
+														)}</span
+													>
 												</a>
 											{/snippet}
 										</Sidebar.MenuButton>
@@ -310,7 +319,7 @@
 					<Sidebar.MenuItem>
 						<Sidebar.MenuButton
 							onclick={handleClearAllOrders}
-							class="bg-red-500/15 hover:bg-red-500/25 text-red-600 dark:text-red-400"
+							class="bg-red-500/15 text-red-600 hover:bg-red-500/25 dark:text-red-400"
 						>
 							{#snippet tooltipContent()}Clear All Orders{/snippet}
 							<Ban />
